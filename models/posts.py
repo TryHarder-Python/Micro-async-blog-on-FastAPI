c28ae941efe9e67111e04c18504721f347d0b0df
Gleb Maximenko <gleb.white90@gmail.com>
2022-01-19 13:07:02 +0200

    Init commit

---
+++ models/posts.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+
+from .users import users_table
+
+
+metadata = sqlalchemy.MetaData()
+
+
+post_table = sqlalchemy.Table(
+    'posts',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column('user_id', sqlalchemy.ForeignKey(users_table.c.id)),
+    sqlalchemy.Column('create_at', sqlalchemy.DateTime()),
+    sqlalchemy.Column('title', sqlalchemy.String(100)),
+    sqlalchemy.Column('content', sqlalchemy.Text()),
+)
